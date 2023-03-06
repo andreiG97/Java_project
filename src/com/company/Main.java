189c235e0f81e39531dc0f9a7df3b48833771392
Andrei G <andrei.grideanu@gmail.com>
2023-03-06 23:39:16 +0200

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    Bank bank = new Bank("ING");
+    bank.addBranch("Expozitiei");
+    bank.addCustomer("Expozitiei", "Paco", 100000);
+    bank.addCustomer("Expozitiei", "Dumbo", 100500);
+    bank.addBranch("Sibiu");
+    bank.addCustomer("Sibiu", "Paty", 20500);
+    bank.addCustomerTransaction("Sibiu", "Paty", 500);
 
-
+    bank.listCustomers("Sibiu", true);
+    bank.listCustomers("Expozitiei", false);
     }
 }
